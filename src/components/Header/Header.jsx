4e2b9be6571625bc780c8937ba0a8550757b9faa
neqxs7 <130995741+neqxs7@users.noreply.github.com>
2024-07-01 17:07:02 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
 
 	return (
 		<nav>
-			<a href="https://neqetria.lol">neqxs</a>
+			<a href="https://neqetria.lol">neqetria.</a>
 			<div className="theme" onClick={() => toggleTheme()}>
 				<i
 					className={`fa-solid fa-${theme === "light-theme" ? "moon" : "sun"}`}
